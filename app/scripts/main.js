3ee78a7376754e343fefce6c3115e3b660404bba
Dylan Swartz <dylan@ivorypenguin.com>
2013-05-18 15:10:11 -0400

    added nytimes/ice for tracking changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,11 +18,28 @@ require.config({
         jquery: '../bower_components/jquery/jquery',
         backbone: '../bower_components/backbone-amd/backbone',
         underscore: '../bower_components/underscore-amd/underscore',
+        ice: './ice.min'
     }
 });
 
 require([
-    'backbone'
+    'backbone',
+    'ice'
 ], function (Backbone) {
     Backbone.history.start();
+
+    jQuery(function() {
+        var tracker = new ice.InlineChangeEditor({
+            // element to track - ice will make it contenteditable
+            element: document.getElementById('editor'),
+            // tell ice to setup/handle events on the `element`
+            handleEvents: true,
+            // set a user object to associate with each change
+            currentUser: { id: 1, name: 'Tutor' }
+        });
+        // setup and start event handling for track changes
+        tracker.startTracking();
+        
+        window.tracker = tracker;
+    });
 });
