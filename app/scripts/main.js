1ec6052aa9d967deb9aec45365d2552b2f13c52c
Dylan Swartz <dylan@ivorypenguin.com>
2013-05-18 17:18:36 -0400

    started making a hemmingway ice plugin

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -28,18 +28,81 @@ require([
 ], function (Backbone) {
     Backbone.history.start();
 
-    jQuery(function() {
-        var tracker = new ice.InlineChangeEditor({
-            // element to track - ice will make it contenteditable
-            element: document.getElementById('editor'),
-            // tell ice to setup/handle events on the `element`
-            handleEvents: true,
-            // set a user object to associate with each change
-            currentUser: { id: 1, name: 'Tutor' }
-        });
-        // setup and start event handling for track changes
-        tracker.startTracking();
+   // jQuery(function() {
+        // hemingway.getCursorPos();
+        window.hemingway = hemingway;
+        jQuery.browser = {
+            version: 'p'
+        };
+    // });
+
+    (function() {
+
+        var exports = this;
+
+        var IceHemingwayPlugin = function(ice_instance) {
+            this._ice = ice_instance;
+        };
+
+        IceHemingwayPlugin.prototype = {
+
+            keyDown: function(e) {
+                console.log("do stuff");
+               return true;
+            }
+
+        };
+
+        ice.dom.noInclusionInherits(IceHemingwayPlugin, ice.IcePlugin);
+        exports._plugin.IceHemingwayPlugin = IceHemingwayPlugin;
+
+    }).call(ice); 
+
+    var hemingway = (function () {
+        var init,
+            tracker,
+            getCursorPos;
+
+        getCursorPos = function (element) {
+            console.log("getCursorPos() call");
+            var caretOffset = 0;
+            if (typeof window.getSelection != "undefined") {
+                var range = window.getSelection().getRangeAt(0);
+                var preCaretRange = range.cloneRange();
+                preCaretRange.selectNodeContents(element);
+                preCaretRange.setEnd(range.endContainer, range.endOffset);
+                caretOffset = preCaretRange.toString().length;
+            } else if (typeof document.selection != "undefined" && document.selection.type != "Control") {
+                var textRange = document.selection.createRange();
+                var preCaretTextRange = document.body.createTextRange();
+                preCaretTextRange.moveToElementText(element);
+                preCaretTextRange.setEndPoint("EndToEnd", textRange);
+                caretOffset = preCaretTextRange.text.length;
+            }
+            return caretOffset;
+        }
+
+        init = function (argument) {
+            console.log("Initilizing hemingway...");
+            tracker = new ice.InlineChangeEditor({
+                // element to track - ice will make it contenteditable
+                element: document.getElementById('editor'),
+                // tell ice to setup/handle events on the `element`
+                handleEvents: true,
+                // set a user object to associate with each change
+                currentUser: { id: 1, name: 'Tutor' },
+                plugins: ['IceEmdashPlugin', 'IceHemingwayPlugin']
+            });
+            // setup and start event handling for track changes
+            tracker.startTracking();
+
+            window.tracker = tracker; 
+        }
         
-        window.tracker = tracker;
-    });
+        init();
+        
+        return {
+            getCursorPos: getCursorPos
+        }
+    })(); 
 });
